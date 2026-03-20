82d245ef30898f5553ced9179487d3f2572d0a86
charcliu <charcliu@tencent.com>
2026-03-21 00:43:24 +0800

    Initial Commit

---
+++ YoniPhoto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YoniPhoto
+//
+//  Created by 刘畅 on 2026/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YoniPhoto/YoniPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoniPhotoApp.swift
+//  YoniPhoto
+//
+//  Created by 刘畅 on 2026/3/21.
+//
+
+import SwiftUI
+
+@main
+struct YoniPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
